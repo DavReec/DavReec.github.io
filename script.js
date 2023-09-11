0c57ee9a491305a5e0f666cf6d3d24ef9642e342
DavReec <144298771+DavReec@users.noreply.github.com>
2023-09-11 11:38:47 -0400

    Update 2 script.js

--- script.js
+++ script.js
@@ -3,34 +3,28 @@ const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
 const MAN = document.getElementById('man');
 
+
+
 function calcFlujo(peso){
     let resto = peso;
     let flujo = 0;  
-    if (peso <=30){
-        if (resto>=20){
-            let aux = resto-20;
-            flujo += aux*1;
-            resto -= aux;
-        } 
-        if (resto>=10){
-            let aux = resto-10;
-            flujo += aux*2;
-            resto -= aux;
-        }
-        flujo += resto*4;
-        return flujo; 
-    }
-    else{
-        peso = parseInt(peso);
-        let flujo =(((peso * 4) + 7) / (peso + 90))*1500;
-        flujo /= 24;
-        return flujo; 
+    if (resto>=20){
+        let aux = resto-20;
+        flujo += aux*1;
+        resto -= aux;
     } 
+    if (resto>=10){
+        let aux = resto-10;
+        flujo += aux*2;
+        resto -= aux;
+    }
+    flujo += resto*4;
+    return flujo;
 }
 
 CALCULAR.addEventListener('click', () => {
-    const DATO = document.getElementById('peso').value
-    if (DATO > 0){
+    const DATO = document.getElementById('peso').valueAsNumber
+    if (DATO > 0 && DATO <= 30){
         ERROR.style.display = 'none'
         let flujo = calcFlujo(DATO);
         let mantenimiento = flujo*1.5;
@@ -38,7 +32,15 @@ CALCULAR.addEventListener('click', () => {
         MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
         FLU.style.display = 'block';
         MAN.style.display = 'block';
-    } else {
+    }else if(DATO > 30){
+        ERROR.style.display = 'none'
+        let SuperficieCorporal = ( (DATO * 4) + 7) / (DATO + 90);
+        FLU.innerHTML = SuperficieCorporal*1500 + ' cc/hr';
+        MAN.innerHTML = SuperficieCorporal*2000 + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+    }else {
+        
         ERROR.style.display = 'block';
         FLU.style.display = 'none';
         MAN.style.display = 'none';
