7495a40e8a32c3ddec99bd0d7fad21a6e95d5e46
DavReec <144298771+DavReec@users.noreply.github.com>
2023-09-11 09:32:29 -0400

    script

---
+++ script.js
@@ -0,0 +1,45 @@
+const CALCULAR = document.getElementById('calcular');
+const ERROR = document.getElementById('error');
+const FLU = document.getElementById('flu');
+const MAN = document.getElementById('man');
+
+function calcFlujo(peso){
+    let resto = peso;
+    let flujo = 0;  
+    if (peso <=30){
+        if (resto>=20){
+            let aux = resto-20;
+            flujo += aux*1;
+            resto -= aux;
+        } 
+        if (resto>=10){
+            let aux = resto-10;
+            flujo += aux*2;
+            resto -= aux;
+        }
+        flujo += resto*4;
+    }
+    else{
+        peso = parseInt(peso);
+        let flujo =(((peso * 4) + 7) / (peso + 90))*1500;
+        flujo /= 24;
+    }
+    return flujo;       
+}
+
+CALCULAR.addEventListener('click', () => {
+    const DATO = document.getElementById('peso').value
+    if (DATO > 0){
+        ERROR.style.display = 'none'
+        let flujo = calcFlujo(DATO);
+        let mantenimiento = flujo*1.5;
+        FLU.innerHTML = flujo + ' cc/hr';
+        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+    } else {
+        ERROR.style.display = 'block';
+        FLU.style.display = 'none';
+        MAN.style.display = 'none';
+    }
+})
